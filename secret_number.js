2a91cf97a93ba903bdfff98dd9637a26fc1fec1e
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-06 10:32:09 -1000

    made suggested changes

--- secret_number.js
+++ secret_number.js
@@ -3,15 +3,11 @@ module.exports = function() {
 
 let num;
 
-// = Math.floor(Math.random()*1000001);
-
 (function randomizer (){
     num = Math.floor(Math.random()*1000001);
 })();
     
 return function (){
-    // let num = Math.floor(Math.random()*1000001);
-    console.log(num);
     return num;
 };
 
