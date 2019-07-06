5e7f961f5aaca3168e99685010a6b5fa59aac7b3
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-06 10:36:01 -1000

    finished edits

--- secret_number.js
+++ secret_number.js
@@ -1,11 +1,7 @@
 'use strict';
 module.exports = function() {
 
-let num;
-
-(function randomizer (){
-    num = Math.floor(Math.random()*1000001);
-})();
+let num = Math.floor(Math.random()*1000001);
     
 return function (){
     return num;
